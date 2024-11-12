5953a2fc29f658aa99e55713437066710dadf388
Michael Duren <141460708+460577@users.noreply.github.com>
2024-11-11 18:14:19 -0600

    feat: setup stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,41 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Building with .NET Aspire",
+      social: {
+        github: "https://github.com/michael-duren",
+        linkedin: "https://www.linkedin.com/in/michael-duren/",
+        blueSky: "https://bsky.app/profile/michaelduren.bsky.social",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Microservices",
+              slug: "guides/microservices",
+            },
+            {
+              label: "What is .NET Aspire",
+              slug: "guides/what-is-dotnet-aspire",
+            },
+            {
+              label: "Secrets & Azure Key Vault",
+              slug: "guides/managing-secrets",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/guides/what-is-dotnet-aspire.md
@@ -0,0 +1,10 @@
+---
+title: What is .NET Aspire?
+description: A high level overview of the new technology from Microsoft.
+---
+
+## What is .NET Aspire?
+
+## Further reading
+
+- Read [about how-to guides](https://diataxis.fr/how-to-guides/) in the Diátaxis framework
